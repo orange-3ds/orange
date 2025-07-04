6f08c1ec4f5be149651c5b2fd1d3ff6f68798edc
Zachary Jones <zjones.092912@gmail.com>
2025-07-04 10:25:01 -0500

    Add project files.

---
+++ orange/Orange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OrangeLib;
+
+namespace Orange
+{
+    static class Orange
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "--help")
+            {
+                Info.Help();
+            }   
+            else if (args[0] == "--version" || args[0] == "-v")
+            {
+                Info.Version();
+            }
+            else
+            {
+                Console.WriteLine("Unknown command. Use --help to view help information");
+            }
+        }
+    }
+}
---
+++ orangelib/OrangeLib.cs
@@ -0,0 +1,15 @@
+﻿namespace OrangeLib
+{
+    static public class Info
+    {
+        static readonly string _version = "0.0.0 Beta";
+        static public void Help()
+        {
+            System.Console.WriteLine("Orange");
+        }
+        static public void Version()
+        {
+            System.Console.WriteLine($"Orange Version {_version}");
+        }
+    }
+}
